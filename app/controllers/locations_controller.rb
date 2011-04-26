2732277355748520f9024c8f96bd2b30eca3f6d8
Mike Shea <mike@mikeshea.net>
2011-04-25 22:28:11 -0400

    Working build

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,12 @@
 class LocationsController < ApplicationController
 	def create
 		@user = User.find(params[:user_id])
-		@location = @user.locations.create(params[:location])
-		redirect_to user_path(@user, :notice => 'Location added.')
-#		if @location.save
-#      format.html { redirect_to user_path(@user, :notice => 'Location added.') }
-#    else
-#      format.html { render :action => "new" }
-#    end
+    if @location = @user.locations.create(params[:location])
+      redirect_to (@user, :notice => 'Location added.')
+    else
+      redirect_to (@user, :notice => 'Location not added.')
+    end
+      
 	end
 	def edit
 		# Note, I have some bullshit routing problem going on here that forces me to use user_id instead of loc id.
