3365ea6546a061c236c2d8d2b7e4fefe737a7358
Mike Shea <mike@mikeshea.net>
2011-04-26 16:44:01 -0400

    Last Workday Build

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	has_many :locations, :order => "updated_at DESC", :limit => 3
+	has_many :locations, :order => "updated_at DESC", :limit => 3, :dependent => :destroy
 	validates_uniqueness_of :email
 	validates_presence_of :email, :name
 end
