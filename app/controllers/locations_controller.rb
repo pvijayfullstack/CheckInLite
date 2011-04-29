1c38be712c54286fffc4a10eda22126917cf707c
Mike Shea <mike@mikeshea.net>
2011-04-29 09:51:03 -0400

    Working build with Google Map

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,11 +4,11 @@ class LocationsController < ApplicationController
 	  @user = User.find(params[:user_id])
 	  @location = @user.locations.create(params[:location])
     if @location.save
-      redirect_to (@user, :notice => 'Location added')
-      #redirect_to (@user), :notice => 'Location added'
+      #redirect_to (@user, :notice => 'Location added')
+      redirect_to (@user), :notice => 'Location added'
     else
-      redirect_to (@user, :notice => 'Location not added, please include all fields.')
-      #redirect_to (@user), :notice => 'Location not added, please include all fields.'
+      #redirect_to (@user, :notice => 'Location not added, please include all fields.')
+      redirect_to (@user), :notice => 'Location not added, please include all fields.'
     end
 	end
 	def edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110427184709) do
+ActiveRecord::Schema.define(:version => 20110429133612) do
 
   create_table "locations", :force => true do |t|
     t.string   "name"
